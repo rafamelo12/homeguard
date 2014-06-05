a0856cf47463481a13b74dd0183d071a1fa31f72
Nelson Antunes <neryuuk@neryuuk.com>
2014-06-05 02:03:24 -0400

    cloudant_connect take picture and put on cloudant
    client\cloudant_conect.py takes 2 pictures and puts them attached to a
    document on cloudant

---
+++ client/cloudant_connect.py
@@ -0,0 +1,69 @@
+import cloudant
+import io
+import picamera
+import time
+from base64 import *
+from mimetypes import *
+from http import client
+from datetime import datetime
+from uuid import *
+
+def new_id():
+    '''() -> str
+    Return a UUID formated as string without dashes.
+    '''
+    return str(uuid4()).replace('-','')
+
+def string64(buff):
+    '''(Buffer) -> str
+    Return a base64 encoded string correctly formated for JSON.
+    '''
+    return str(b64encode(buff.read()))[2:-1]
+
+# Cloudant account information
+ACCOUNT = 'neryuuk'
+USER = 'cenditheroddemingiviceds'
+PASS = 'JHHEEBQm17EU3RaGo6mbY6JY'
+DB = 'homeguard'
+
+account = cloudant.Account(ACCOUNT)
+
+login = account.login(USER, PASS)
+assert login.status_code == client.OK
+print('HTTP status:',login.status_code,client.responses[login.status_code])
+
+#file_name = 'image.jpg'
+#file_mime = guess_type(file_name)[0]
+#pict = open(file_name,'rb')
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.exposure_mode = 'auto'
+    camera.resolution = (1366, 768)
+    camera.vflip = True
+    time.sleep(5)
+    camera.capture('file.jpg')
+    camera.capture(stream, 'jpeg')
+img_file = open('file.jpg','rb')
+print('\'file.jpg\' captured!')
+stream.seek(0)
+print('\'stream.jpg\' captured!')
+
+db = account.database(DB)
+doc = db.document(new_id())
+
+response = doc.put(params = {
+    'utc_timestamp': str(datetime.utcnow()),
+    'local_timestamp': str(datetime.now()),
+    '_attachments': {
+        'stream.jpg': {
+            'content_type': 'image/jpeg',
+            'data': string64(stream)
+        },
+        'file.jpg': {
+            'content_type': 'image/jpeg',
+            'data': string64(img_file)
+        }
+    }
+})
+
+print(response.json())
--- server/nodeClient.js
+++ server/nodeClient.js
@@ -1,28 +1,33 @@
 var net = require('net');
+var bl = require('bl');
 
-var py_host = '138.51.229.242' //IP
-var py_port = 5000
+var py_host = '138.51.58.186'; //IP
+var py_port = 5000;
 
 var client = new net.Socket();
 
-//client.setEnconding('base64');
+// client.setEncoding('base64');
 
-client.setTimeout(10000);
+client.setTimeout(1000);
 
 client.connect(py_port, py_host, function() {
-    console.log('connected to server ' + py_host + ':' + py_port + '...');
+    console.log('Connected to ' + py_host + ':' + py_port + '...');
     client.write('i\'m your client, bitch!');
 });
 
-client.on('data', function(data) {
-    console.log('data received: ' + data);
-    client.destroy();
+client.on('data', function() {
+	client.pipe(bl(function(err,data) {
+		if(err)
+			console.error(err);
+		console.log('Data received: ' + data);
+	}));
+    //client.destroy();
 });
 
 client.on('close', function() {
-    console.log('connection with server closed.')
+    console.log('Connection closed.\n');
 });
 
 client.on('timeout', function() {
-    console.log('timeout!');
+    console.log('Timeout.\n');
 })
