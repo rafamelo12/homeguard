d19ac177bbac7a11b6c071c3a3b9b6b1f8677292
Higor Ernandes <higorernandes@gmail.com>
2014-06-26 16:18:12 -0400

    Navigation Drawer with menu is opening. Login screen is opening MainActivity.

--- src/com/example/homeguard/MainActivity.java
+++ src/com/example/homeguard/MainActivity.java
@@ -1,112 +1,108 @@
 package com.example.homeguard;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.VideoView;
 
-//public class MainActivity extends ActionBarActivity
-//        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity 
+	implements NavigationDrawerFragment.NavigationDrawerCallbacks{
+	
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+	
+	private CharSequence mTitle;
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-//    private NavigationDrawerFragment mNavigationDrawerFragment;
+	public void onCreate(Bundle savedInstanceState){
+		
+//		//Bad debugging technique:
+//        Toast.makeText(getApplicationContext(), "Potato.", Toast.LENGTH_SHORT).show();
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mNavigationDrawerFragment = (NavigationDrawerFragment)
+				getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+		mTitle = getTitle();
+		
+		VideoView stream = (VideoView) findViewById(R.layout.stream);
+		LinearLayout deviceTitle = (LinearLayout) findViewById(R.layout.device_title);
+		RelativeLayout activeDeviceItem = (RelativeLayout) findViewById(R.layout.active_device_item);
+		RelativeLayout idleDeviceItem = (RelativeLayout) findViewById(R.layout.idle_device_item);
+		RelativeLayout unreachableDeviceItem = (RelativeLayout) findViewById(R.layout.unreachable_device_item);
+		
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity); 
+		// Set up the drawer.
+		mNavigationDrawerFragment.setUp(
+				R.id.navigation_drawer,	
+				(DrawerLayout) findViewById(R.id.drawer_layout));
+	}
 
-//        mNavigationDrawerFragment = (NavigationDrawerFragment)
-//                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-//        mTitle = getTitle();
+	public void onNavigationDrawerItemSelected(int position) {
+		// update the main content by replacing fragments
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		fragmentManager.beginTransaction()
+			.replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+			.commit();
+	}
 
-//        // Set up the drawer.
-//        mNavigationDrawerFragment.setUp(
-//                R.id.navigation_drawer,
-//                (DrawerLayout) findViewById(R.id.drawer_layout));
-    }
+	public void onSectionAttached(int number) {
+		switch (number) {
+		case 1:
+			mTitle = getString(R.string.screenshot);
+			break;
+		case 2:
+			mTitle = getString(R.string.settings);
+			break;
+		case 3:
+			mTitle = getString(R.string.logout);
+			break;
+		}
+	}
+	
+	public void restoreActionBar() {
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+	}
 
-//    @Override
-//    public void onNavigationDrawerItemSelected(int position) {
-//        // update the main content by replacing fragments
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        fragmentManager.beginTransaction()
-//                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-//                .commit();
-//    }
-
-//    public void onSectionAttached(int number) {
-//        switch (number) {
-//            case 1:
-//                mTitle = getString(R.string.title_section1);
-//                break;
-//            case 2:
-//                mTitle = getString(R.string.title_section2);
-//                break;
-//            case 3:
-//                mTitle = getString(R.string.title_section3);
-//                break;
-//        }
-//    }
-
-//    public void restoreActionBar() {
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-//        actionBar.setDisplayShowTitleEnabled(true);
-//        actionBar.setTitle(mTitle);
-//    }
-
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-//            // Only show items in the action bar relevant to this screen
-//            // if the drawer is not showing. Otherwise, let the drawer
-//            // decide what to show in the action bar.
-//            getMenuInflater().inflate(R.menu.main, menu);
-//            restoreActionBar();
-//            return true;
-//        }
-//        return super.onCreateOptionsMenu(menu);
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		if (!mNavigationDrawerFragment.isDrawerOpen()) {
+			//	Only show items in the action bar relevant to this screen
+			// if the drawer is not showing. Otherwise, let the drawer
+			// decide what to show in the action bar.
+			getMenuInflater().inflate(R.menu.main, menu);
+			restoreActionBar();
+			return true;
+		}
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		//Settings menu at the top bar
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			Intent settings = new Intent(MainActivity.this,SettingsActivity.class);
+			startActivity(settings);
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public static class PlaceholderFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -137,12 +133,11 @@ public class MainActivity extends ActionBarActivity {
             return rootView;
         }
 
-//        @Override
-//        public void onAttach(Activity activity) {
-//            super.onAttach(activity);
-//            ((MainActivity) activity).onSectionAttached(
-//                    getArguments().getInt(ARG_SECTION_NUMBER));
-//        }
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
     }
-
 }
